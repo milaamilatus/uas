814466373622ce85f26c95dcf22a594f1cd2c95a
unknown <amilatussholehah6@gmail.com>
2023-12-29 14:48:11 +0700

    push ke-11

--- project_saya/urls.py
+++ project_saya/urls.py
@@ -4,5 +4,8 @@ from toko.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('buah',buah, name='buah')
+    path('buah',buah, name='buah'),
+    path('beli',beli, name='beli'),
+    path('total',total, name='total'),
+
 ]
--- toko/views.py
+++ toko/views.py
@@ -4,4 +4,11 @@ from django.http import HttpResponse
 def buah(request):
     return render(request, 'myweb/buah.html')
 
+def beli(request):
+    return render(request, 'myweb/beli.html')
+
+def total(request):
+    return render(request, 'myweb/total.html')
+
+
 
