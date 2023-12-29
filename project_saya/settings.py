5bda0c3585af194088dc238f7b5eaa936720f969
unknown <amilatussholehah6@gmail.com>
2023-12-29 08:35:35 +0700

    push ke-6

--- project_saya/settings.py
+++ project_saya/settings.py
@@ -113,5 +113,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, )
+
 
 # Add these new lines
--- project_saya/urls.py
+++ project_saya/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from web.views import *
+from toko.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
